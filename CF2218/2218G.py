6f2ea3af381246900498bc21b5129e344f53d79d
jushangxu <jushangxu@outlook.com>
2026-04-09 20:38:26 +0800

    update CF2217

--- CF2218/2218G.py
+++ CF2218/2218G.py
@@ -7,23 +7,29 @@ def solve():
     inf = 10**9
     n, m = map(int, input().split())
     a = list(map(int , input().split()))
-    mp = Counter(a)
-    pre = [0] * (m + 5)
-    for i in range(1, m + 1):
+  
+    mp = [0] * (max(a + [0]) + 5)
+    for x in a:
+        mp[x] += 1
+        
+    pre = [0] * len(mp)
+    for i in range(1, len(mp)):
         pre[i] = pre[i - 1] + mp[i - 1] 
+        
     ans = 1
     a = [inf] + a + [inf]
     for i in range(1, n + 1):
-        if not a[i]:
+        val = a[i]
+        if not val:
             continue
         tag = min(a[i - 1], a[i + 1])
-        if tag >= a[i]:
+        if tag >= val:
             print(0)
             return
-        if a[i] != tag + 1:
-            ans = (ans * mp[a[i] - 1]) % mod
+        if val != tag + 1:
+            ans = (ans * mp[val - 1]) % mod
         else:
-            ans = (ans * pre[a[i]]) % mod
+            ans = (ans * pre[val]) % mod
     print(ans)
     
 for _ in range(int(input())):
