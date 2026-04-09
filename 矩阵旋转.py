7e3f64dd53725c70a5ab45e63fdeddad922dded1
DESKTOP-3D4753M\JuShang XU <jushangxu@outlook.com>
2026-04-09 20:38:26 +0800

    update CF2217

---
+++ 矩阵旋转.py
@@ -0,0 +1,29 @@
+import sys
+input = lambda: sys.stdin.readline().strip()
+
+def change(a, b, l):
+    return b, l - 1 - a
+
+def main():
+    n = int(input())
+    q = int(input())
+    ops = []
+    for _ in range(q):
+        op, x, y, l = map(int, input().split())
+        ops.append((op, x, y, l))
+
+    d = int(input())
+    for _ in range(d):
+        x, y = map(int, input().split())
+        for op, x0, y0, l in reversed(ops):
+            if x0 <= x < x0 + l and y0 <= y < y0 + l:
+                a, b = x - x0, y - y0
+                if op == 1:
+                    for _ in range(3):
+                        a, b = change(a, b, l)
+                else:
+                    a, b = change(a, b, l)
+                x, y = x0 + a, y0 + b
+        print((x - 1) * n + y)
+
+main()
