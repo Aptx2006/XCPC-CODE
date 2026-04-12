e2766746f4e032cc7528d3ce481595d6cfb7da9f
jushangxu <jushangxu@outlook.com>
2026-04-12 15:22:29 +0800

    update春季联赛4

--- 练习.py
+++ 练习.py
@@ -1,50 +1,32 @@
 import sys
-it = map(int, sys.stdin.read().split())
-II = lambda: next(it)
+input = lambda: sys.stdin.readline().strip()
+
+def fun(total, length):
+    if length == 1:
+        return [(total,)]
+    res = []
+    for x in range(total + 1):
+        for rest in fun(total - x, length - 1):
+            res.append((x,) + rest)
+    return res
+
 
 def main():
-    n = II()
-    m = II()
-    fa = list(range(n))
-    rank = [0] * n
+    L, N = map(int, input().split())
+    
+    a = fun(L, N)
+    tag = tuple([L] * N)
+    dp = {tuple([0] * N): 1}
+    
+    for _ in range(N):
+        ndp = {}
+        for c, cnt in dp.items():
+            for row in a:
+                nsum = tuple(c[i] + row[i] for i in range(N))
+                if all(x <= L for x in nsum):
+                    ndp[nsum] = ndp.get(nsum, 0) + cnt
+        dp = ndp
+        
+    print(dp.get(tag, 0))
 
-    def find(x):
-        while fa[x] != x:
-            fa[x] = fa[fa[x]]
-            x = fa[x]
-        return x
-
-    def union(x, y):
-        rx = find(x)
-        ry = find(y)
-        if rx == ry:
-            return False
-        if rank[rx] < rank[ry]:
-            fa[rx] = ry
-        else:
-            fa[ry] = rx
-            if rank[rx] == rank[ry]:
-                rank[rx] += 1
-        return True
-
-    ans = 0
-    op = []
-    for _ in range(m):
-        p = II()
-        u = II() - 1
-        v = II() - 1
-        w = II()
-        if p == 1:
-            ans += w
-            union(u, v)
-        else:
-            op.append((w, u, v))
-
-    op.sort()
-    for w, u, v in op:
-        if union(u, v):
-            ans += w
-    print(ans)
-
-for _ in range(1):
-    main()
+main()
